8054ac804b10ae7f7e11e91479c6e4b22e804187
Dominik Blank <DominikPBlank@t-online.de>
2019-02-05 19:40:15 +0100

    Added hangman images and max number of guesses

--- hangman.py
+++ hangman.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox, simpledialog
+from PIL import Image, ImageTk
+import os
 import random
 import re
 
@@ -22,13 +24,21 @@ def initialize_game(*args):
     # Create placeholder text to display
     guess_word = "-" * len(word)
     word_label.config(text=guess_word)
+    # Start display of hangman image
+    hangman_img = ImageTk.PhotoImage(Image.open(os.path.dirname(__file__)+"/pictures/Hangman-0.png"))
+    hangman_label.config(image=hangman_img)
+    hangman_label.image = hangman_img
+    # Delete text from the result label
+    result_label.config(text="")
     return word, guess_word
 
 def hangman(*args):
     # Call function that picks random word and displays the placeholder text
     word, guess_word = initialize_game()
+    # Keep track of number of guessed characters
+    guesses = 0
     # The actual game starts here
-    while word != guess_word:
+    while word != guess_word and guesses < 6:
         # Ask for string input (should be a character only) and put the input in lower case
         guess_char = simpledialog.askstring("Input", "Guess a character:", parent=root).lower()
         # Check if only one character was entered
@@ -43,12 +53,22 @@ def hangman(*args):
             # Check if the character occurs in the word
             if len(char_index) == 0:
                 messagebox.showinfo("Try again!", "The word doesn't contain that character! Try again!")
+                # Increase the guess counter
+                guesses += 1
+                # Update the displayed hangman image
+                hangman_img = ImageTk.PhotoImage(Image.open(os.path.dirname(__file__) + "/pictures/Hangman-" + str(guesses) + ".png"))
+                hangman_label.config(image=hangman_img)
+                hangman_label.image = hangman_img
             else:
                 # Replace the dashes with the guessed character
                 for i in char_index:
                     guess_word = guess_word[:i] + guess_char + guess_word[i+1:]
                 # Update the displayed text
                 word_label.config(text=guess_word)
+    if word != guess_word:
+        result_label.config(text="You lost. The word was " + word + ".")
+    elif guesses == 6:
+        result_label.config(text="You won. You solved it with " + str(guesses) + " guesses.")
 
 #====================
 
@@ -57,14 +77,18 @@ root = Tk()
 # Set the window title
 root.title("Hangman - The Game")
 # Set window size
-root.geometry("512x384")
+root.geometry("512x768")
 
 #====================
 
-# Create text labels (start, exit)
+# Create text labels (start, exit, word)
 start_label = Label(root, text="Start the game:")
 exit_label = Label(root, text="Close the window:")
 word_label = Label(root, font=("times", 20))
+result_label = Label(root, font=("times", 20))
+
+# Create image label
+hangman_label = Label(root)
 
 # Create buttons (start, exit)
 start_button = Button(root, text="Start", command=hangman)
@@ -77,6 +101,10 @@ exit_button = Button(root, text="Exit", command=root.destroy)
 start_label.place(x = 106, y = 10, width=150, height=30)
 exit_label.place(x = 106, y = 50, width=150, height=30)
 word_label.place(x = 0, y = 130, width=512, height=60)
+result_label.place(x = 0, y = 486, width=512, height=60)
+
+# Image label
+hangman_label.place(x = 0, y = 210, width=512, height=256)
 
 # Buttons
 start_button.place(x = 256, y = 10, width=150, height=30)
